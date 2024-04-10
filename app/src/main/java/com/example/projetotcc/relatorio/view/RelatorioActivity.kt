1329bfca1705051330e47cdc2938a04e547ea915
mairyeduardo <mairyeduardo@gmail.com>
2024-04-09 23:42:40 -0300

    adicionando mudanças de telas entre os menus

--- app/src/main/java/com/example/projetotcc/relatorio/view/RelatorioActivity.kt
+++ app/src/main/java/com/example/projetotcc/relatorio/view/RelatorioActivity.kt
@@ -1,8 +1,15 @@
 package com.example.projetotcc.relatorio.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projetotcc.cliente.view.ClienteActivity
 import com.example.projetotcc.databinding.ActivityRelatorioBinding
+import com.example.projetotcc.home.view.HomeActivity
+
+private const val DELAY_TELA = 1000L
 
 class RelatorioActivity : AppCompatActivity() {
 
@@ -14,5 +21,34 @@ class RelatorioActivity : AppCompatActivity() {
         getSupportActionBar()?.hide()
         setContentView(binding.root)
 
+        configurarBotaoCliente()
+        configurarBotaoHome()
     }
+
+    private fun chamarTelaHome() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@RelatorioActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }, DELAY_TELA)
+    }
+
+    private fun chamarTelaCliente() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@RelatorioActivity, ClienteActivity::class.java)
+            startActivity(intent)
+        }, DELAY_TELA)
+    }
+
+    private fun configurarBotaoHome() {
+        binding.ivServico.setOnClickListener {
+            chamarTelaHome()
+        }
+    }
+
+    private fun configurarBotaoCliente() {
+        binding.ivCliente.setOnClickListener{
+            chamarTelaCliente()
+        }
+    }
+
 }

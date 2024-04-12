6d695b99ecafd3f5b34b732a84c4d7a1e2a6bf1d
mairyeduardo <mairyeduardo@gmail.com>
2024-04-12 19:30:30 -0300

    direcionamento para detalhes do serviço pendente

--- app/src/main/java/com/example/projetotcc/servico/view/DetalhesServicoPendente.kt
+++ app/src/main/java/com/example/projetotcc/servico/view/DetalhesServicoPendente.kt
@@ -1,8 +1,11 @@
 package com.example.projetotcc.servico.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.projetotcc.databinding.ActivityDetalhesServicoPendenteBinding
+import com.example.projetotcc.home.domain.ServicoModel
 
 private const val DELAY_TELA = 1000L
 
@@ -15,6 +18,21 @@ class DetalhesServicoPendente : AppCompatActivity() {
         binding = ActivityDetalhesServicoPendenteBinding.inflate(layoutInflater)
         getSupportActionBar()?.hide()
         setContentView(binding.root)
+
+        val servico = intent.extras?.getSerializable("servico") as? ServicoModel
+        if (servico == null) {
+            finish()
+            return
+        }
+        binding.textTitulo.text = servico.titulo
+        binding.textDescricao.text = servico.descricao
+        binding.textEndereco.text = servico.enderecoServico
+        binding.textValor.text = servico.valorServico.toString()
+        binding.textCusto.text = servico.custoAtual.toString()
+        binding.textData.text = servico.dataInicio
+        binding.textNomeCliente.text = servico.nomeCliente
+        Log.e("teste", servico.toString())
+
 
     }
 }

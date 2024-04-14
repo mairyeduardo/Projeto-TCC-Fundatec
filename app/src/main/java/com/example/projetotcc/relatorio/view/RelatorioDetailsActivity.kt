41e8a25dd938f2ea28baaf540a7577210b9cab1e
mairyeduardo <mairyeduardo@gmail.com>
2024-04-14 20:46:50 -0300

    direcionando relatorio para os menus

--- app/src/main/java/com/example/projetotcc/relatorio/view/RelatorioDetailsActivity.kt
+++ app/src/main/java/com/example/projetotcc/relatorio/view/RelatorioDetailsActivity.kt
@@ -1,8 +1,13 @@
 package com.example.projetotcc.relatorio.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projetotcc.cliente.view.ClienteActivity
 import com.example.projetotcc.databinding.ActivityRelatorioIndividualBinding
+import com.example.projetotcc.home.view.HomeActivity
 import com.example.projetotcc.relatorio.domain.RelatorioModel
 
 private const val DELAY_TELA = 1000L
@@ -35,5 +40,48 @@ class RelatorioDetailsActivity : AppCompatActivity(){
         binding.despesasTotais.text = relatorio.custoAtual.toString()
         binding.liquidoTotal.text = relatorio.valorLiquido.toString()
 
+        configurarBotaoRelatorio()
+        configurarBotaoCliente()
+        configurarBotaoHome()
     }
+
+    private fun chamarTelaRelatorio() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@RelatorioDetailsActivity, RelatorioActivity::class.java)
+            startActivity(intent)
+        }, DELAY_TELA)
+    }
+
+    private fun chamarTelaCliente() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@RelatorioDetailsActivity, ClienteActivity::class.java)
+            startActivity(intent)
+        }, DELAY_TELA)
+    }
+
+    private fun chamarTelaHome() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@RelatorioDetailsActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }, DELAY_TELA)
+    }
+
+    private fun configurarBotaoRelatorio() {
+        binding.ivRelatorio.setOnClickListener {
+            chamarTelaRelatorio()
+        }
+    }
+
+    private fun configurarBotaoCliente() {
+        binding.ivCliente.setOnClickListener {
+            chamarTelaCliente()
+        }
+    }
+
+    private fun configurarBotaoHome() {
+        binding.ivServico.setOnClickListener {
+            chamarTelaHome()
+        }
+    }
+
 }
